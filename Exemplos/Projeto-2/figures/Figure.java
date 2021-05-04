44c1d3c7a02ba69a1520b0b7b5ff9823928514c4
fsantanna <francisco.santanna@gmail.com>
2021-05-04 12:15:04 -0300

    ...

---
+++ Exemplos/Projeto-2/figures/Figure.java
@@ -0,0 +1,24 @@
+package figures;
+
+import ivisible.IVisible;
+
+public abstract class Figure implements IVisible {
+    public int x, y;
+    public int w, h;
+
+    public Figure (int x, int y, int w, int h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    public void drag (int dx, int dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    public boolean clicked (int x, int y) {
+        return this.x<=x && x<=this.x+this.w && this.y<=y && y<=this.y+this.h;
+    }
+}
---
+++ Exemplos/Projeto-2/ivisible/IVisible.java
@@ -0,0 +1,8 @@
+package ivisible;
+
+import java.awt.Graphics;
+
+public interface IVisible {
+    public boolean clicked (int x, int y);
+    public void    paint   (Graphics g);
+}
